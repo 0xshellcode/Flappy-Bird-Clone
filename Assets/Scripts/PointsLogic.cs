47a5221867a935380d870d9c72138a684a08b5e2
0xshellcode <38792094+0xshellcode@users.noreply.github.com>
2020-12-13 10:28:00 -0600

    Add files via upload

---
+++ Assets/Scripts/PointsLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PointsLogic : MonoBehaviour
+{
+    public static int points = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<TextMeshProUGUI>().text=points.ToString();
+        
+    }
+}
